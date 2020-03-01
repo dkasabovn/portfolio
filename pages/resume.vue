610dd27d754e2273c57e1a6d5812ab29c2e02372
dkasabovn <dkasabovn@gmail.com>
2020-03-01 15:46:29 -0600

    release soon?

--- pages/resume.vue
+++ pages/resume.vue
@@ -2,24 +2,24 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-4 col-sm-12">
-                <b-card class="block mb-3">
+                <b-card class="h-100">
                     <h3>Robotics</h3>
-                    <p>Software lead on QuadX 6299. Check out my 2019-2020 Repository</p>
+                    <p>Software lead on QuadX 6299. Check out my <a href="https://github.com/QuadX6299/2019-2020" target="_blank" class="red-link">2019-2020 Repository</a></p>
                     <h6>Technologies</h6>
                     <ul>
                         <li>Java</li>
                         <li>Kotlin</li>
-                        <li>Revextensions 2</li>
-                        <li>FTC Dashboard</li>
-                        <li>Fully Custom Pure-Pursuit</li>
-                        <li>3 Tracking Wheel Odometry</li>
-                        <li>Holonomic Control</li>
+                        <li><a href="https://github.com/OpenFTC/RevExtensions2" class="lk-link" target="_blank">Revextensions 2</a></li>
+                        <li><a href="https://github.com/acmerobotics/ftc-dashboard" class="lk-link" target="_blank">FTC Dashboard</a></li>
+                        <li><a href="https://github.com/QuadX6299/2019-2020/tree/master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lib" class="lk-link" target="_blank">Fully Custom Pure-Pursuit</a></li>
+                        <li><a href="https://github.com/QuadX6299/2019-2020/blob/master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lib/Odometry/Odom1.kt" class="lk-link" target="_blank">3 Tracking Wheel Odometry</a></li>
+                        <li><a href="https://github.com/QuadX6299/2019-2020/blob/master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lib/Path/PureUtil.kt" class="lk-link" target="_blank">Holonomic Control</a></li>
                     </ul>
                     <b-table borderless :items="robotics"></b-table>
                 </b-card>
             </div>
             <div class="col-lg-4 col-sm-12">
-                <b-card class="block mb-3">
+                <b-card class="h-100">
                     <h3>Academics</h3>
                     <p>Class of 2021 at Vandegrift HS</p>
                     <h6>Classes Taken</h6>
@@ -36,28 +36,32 @@
                 </b-card>
             </div>
             <div class="col-lg-4 col-sm-12">
-                <b-card class="block mb-3">
+                <b-card class="h-100">
                     <h3>Competitive Programming</h3>
-                    <p>I partake in USACO and Comp Sci UIL</p>
+                    <p>I partake in <a href="http://www.usaco.org/index.php" class="red-link" target="_blank">USACO</a> and Comp Sci UIL</p>
                     <h6>Training Used</h6>
                     <ul>
-                        <li>Competitive Programming 3 Steven Halim</li>
+                        <li><a href="https://cpbook.net/" class="lk-link" target="_blank">Competitive Programming 3 Steven Halim</a></li>
                         <li>LeetCode</li>
                         <li>Hackerrank</li>
                         <li>USACO Training</li>
                     </ul>
                     <b-table borderless :items="compsci"></b-table>
                 </b-card>
-                <b-card class="block">
+            </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-lg-4 col-sm-12">
+                <b-card class="h-100">
                     <h3>Self Learning</h3>
                     <p>To acquire new skills I take online courses or read books</p>
                     <h6>Resources</h6>
                     <ul>
-                        <li>Udemy Robotics</li>
-                        <li>Competitive Programming 3</li>
+                        <li><a href="https://classroom.udacity.com/courses/cs373" class="lk-link" target="_blank">Udemy Robotics</a></li>
+                        <li><a href="https://cpbook.net/" class="lk-link" target="_blank">Competitive Programming 3</a></li>
                         <li>Nuxt.js Documentation</li>
                         <li>USACO Training</li>
-                        <li>Cracking the Coding Interview</li>
+                        <li><a href="https://www.amazon.com/Cracking-Coding-Interview-Programming-Questions/dp/0984782850" class="lk-link" target="_blank">Cracking the Coding Interview</a></li>
                     </ul>
                 </b-card>
             </div>
@@ -96,4 +100,9 @@ export default {
 </script>
 
 <style scoped>
+.lk-link {
+    color: rgba(0, 0, 0, .7);
+    text-decoration: underline;
+    cursor: pointer;
+}
 </style>
