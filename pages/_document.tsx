861f2fa97354de936dec0465e32d64359cdb8cb1
dkasabovn <dkasabovn@gmail.com>
2021-04-14 17:49:33 -0500

    added ga

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ import Document, {
 	NextScript,
 	DocumentContext,
 } from "next/document";
+import { Fragment } from "react";
 
 class MyDocument extends Document {
 	static async getInitialProps(ctx: DocumentContext) {
@@ -12,7 +13,20 @@ class MyDocument extends Document {
 		return { ...initialProps };
 	}
 
+	// spooky code
+	injectGA(gaID) {
+		return {
+			__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+			new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+			j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+			'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+			})(window,document,'script','dataLayer','${gaID}');`,
+		};
+	}
+
 	render() {
+		const isProduction = !this.props.isDevelopment;
+		const ga_tracker = process.env.GA_ID;
 		return (
 			<Html lang="en">
 				<Head>
@@ -34,6 +48,15 @@ class MyDocument extends Document {
 				<body className="dark:bg-black">
 					<Main />
 					<NextScript />
+					{isProduction && (
+						<Fragment>
+							<script
+								dangerouslySetInnerHTML={this.injectGA(
+									ga_tracker
+								)}
+							/>
+						</Fragment>
+					)}
 				</body>
 			</Html>
 		);
